886cf1083b57e08b553f108ca3260c034d4cb09d
EthanNC <ethan.n.cumming@gmail.com>
2024-02-03 13:38:55 -0500

    remove http endpoint

--- convex/http.ts
+++ convex/http.ts
@@ -12,25 +12,10 @@ export const newGame = httpAction(async (ctx, request) => {
   return new Response(data, { status: 200 });
 });
 
-export const joinGame = httpAction(async (ctx, request) => {
-  const { gameId, name } = await request.json();
-  await ctx.runAction(api.actions.createUser, {
-    name: name,
-    gameId: gameId,
-  });
-  return new Response(null, { status: 200 });
-});
-
 http.route({
   path: "/newGame",
   method: "POST",
   handler: newGame,
-});
-
-http.route({
-  path: "/joinGame",
-  method: "POST",
-  handler: joinGame,
 });
 
 export default http;

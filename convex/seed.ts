35286c160bd4729bc135abad7781bd815f58e8e6
John Brown <jdb.johnbrown@gmail.com>
2024-02-09 12:59:15 -0500

    Add royal avatars

--- convex/seed.ts
+++ convex/seed.ts
@@ -8,12 +8,12 @@ type user = {
 };
 
 const AvatarArray = [
-  "capybara.webp",
-  "cat.png",
-  "dogo.png",
-  "frog.png",
-  "gorilla.png",
-  "sloth.webp",
+  "King1.png",
+  "Duke2.png",
+  "Queen3.png",
+  "Bear4.png",
+  "King5.png",
+  "Queen6.png"
 ];
 
 export default internalMutation({
